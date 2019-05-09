70816d3da4eada89a26ff87029b42e90d18b07ed
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-09 11:39:41 +0200

    Add the custom _app.js and _document.js files; support for Styled Components server side rendering with Next.js

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import App, { Container } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from '../components/styled'
+
+class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    )
+  }
+}
+
+export default MyApp
