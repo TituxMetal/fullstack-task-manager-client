6732c85e8e91e3a698e13bebf46bfac4eebc6dad
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-11 18:55:13 +0200

    Add HtmlHead component; display the title tag, add some meta tags and favicon link tags

---
+++ components/layout/HtmlHead.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+const HtmlHead = props => {
+  const { title } = props
+
+  return (
+    <>
+      <Head>
+        <title>Fullstack Task Manager{title && ` | ${title}`}</title>
+
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
+        />
+
+        <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
+      </Head>
+    </>
+  )
+}
+
+export default HtmlHead
