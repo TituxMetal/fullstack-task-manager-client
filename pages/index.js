2667a7a115e894100ef8a47a3c9137e1444573ee
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-12 19:01:24 +0200

    Update the Home page; add the TaskContext and List component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import { Page } from '../components/layout'
+import { useContext } from 'react'
 
-const Home = () => (
-  <Page title='Home page'>
-    <h1>Fullstack Task Manager</h1>
-  </Page>
-)
+import { TaskContext } from '../context'
+import { Page } from '../components/layout'
+import { List } from '../components/tasks'
+
+const Home = () => {
+  const { list, loading } = useContext(TaskContext)
+
+  return (
+    <Page title='Home page'>
+      <h1>Fullstack Task Manager</h1>
+      {loading ? <p>Loading data...</p> : list.length > 0 && <List />}
+    </Page>
+  )
+}
 
 export default Home
