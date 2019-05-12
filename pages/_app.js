180d9745871c8bacf45e924b8be13d820a689caa
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-12 18:58:02 +0200

    Update _app.js file; add the ContextProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App, { Container } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
+import { ContextProvider } from '../context'
 import { theme } from '../components/styled'
 
 class MyApp extends App {
@@ -18,9 +19,11 @@ class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <Container>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <ContextProvider>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ContextProvider>
       </Container>
     )
   }
