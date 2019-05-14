094a71bfb5c188a11cd60696a07daca6fd4a5c38
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-14 18:03:11 +0200

    Update Home page; add the title that shows the number of tasks

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import { useContext } from 'react'
 import { TaskContext } from '../context'
 import { Page } from '../components/layout'
 import { List, Form } from '../components/tasks'
+import { Title } from '../components/styled'
 
 const Home = () => {
   const { list, loading } = useContext(TaskContext)
+  const pluralize = `${list.length > 1 ? 'tasks' : 'task'}`
+  const tasksNb = list.length > 0 ? `${list.length} ${pluralize} left` : `Nothing to do!`
 
   return (
     <Page title='Home page'>
-      <h1>Fullstack Task Manager</h1>
+      <Title>{tasksNb}</Title>
       <Form />
       {loading ? <p>Loading data...</p> : list.length > 0 && <List />}
     </Page>
