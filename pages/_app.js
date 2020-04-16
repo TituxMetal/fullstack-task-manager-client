1277e4b53ca295fd6cba29c819d49ddfb6615061
Titux Metal <github@lgdweb.fr>
2020-04-16 03:59:09 +0200

    Update _app.js file; remove getInitialProps static method and Container to avoid nextjs deprecation warnings

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,19 @@
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
 import { ContextProvider } from '../context'
 import { theme } from '../components/styled'
 
 class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
 
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Container>
         <ContextProvider>
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
         </ContextProvider>
-      </Container>
     )
   }
 }
