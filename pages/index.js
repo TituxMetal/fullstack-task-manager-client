9825a56222b4d54ba1ef349406a08845f783bd34
Titux Metal <github@lgdweb.fr>
2020-04-19 07:51:09 +0200

    [REFACTOR] Update index page; fetch data in the getStaticProps function and populate the context in useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 
 import { TaskContext } from '../context'
 import { Page } from '../components/layout'
+import { getAllTasks } from '../lib/query'
 import { List, Form } from '../components/tasks'
 import { Title } from '../components/styled'
 
-const Home = () => {
-  const { list, loading } = useContext(TaskContext)
-  const pluralize = `${list.length > 1 ? 'tasks' : 'task'}`
-  const tasksNb = list.length > 0 ? `${list.length} ${pluralize} left` : `Nothing to do!`
+const Home = ({ data }) => {
+  const { list, fetchData, loading } = useContext(TaskContext)
+  const uncompletedTasks = list.filter(task => task.completed === false)
+  const pluralize = `${uncompletedTasks.length > 1 ? 'tasks' : 'task'}`
+  const tasksNb = uncompletedTasks.length > 0 ? `${uncompletedTasks.length} ${pluralize} left` : `Nothing to do!`
+
+  useEffect(() => {
+    fetchData()
+  }, [data])
 
   return (
     <Page title='Home page'>
@@ -17,6 +23,16 @@ const Home = () => {
       {loading ? <p>Loading data...</p> : list.length > 0 && <List />}
     </Page>
   )
+}
+
+export async function getStaticProps() {
+  const { data } = await getAllTasks()
+
+  return {
+    props: {
+      data,
+    },
+  }
 }
 
 export default Home
