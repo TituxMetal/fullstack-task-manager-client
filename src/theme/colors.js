12b40a37f490baef589f04ffbcb0e2d328053211
Titux Metal <github@lgdweb.fr>
2020-06-18 09:14:41 +0200

    Updating colors.js file in theme; some colors variables for the theme

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,3 +1,19 @@
+export const blackAlpha3 = 'hsla(0, 0%, 0%, 0.3)'
+
+export const cornflowerBlue = 'hsl(218.5, 79.2%, 66.1%)'
+
+export const crimson = 'hsl(348, 83.3%, 47.1%)'
+
+export const darkGoldenrod = 'hsl(42.7, 88.7%, 38.2%)'
+
+export const limeGreen = 'hsl(88, 65.9%, 40.2%)'
+
+export const paleGoldenrod = 'hsl(54.7, 66.7%, 80%)'
+
 export const primaryColor = 'hsl(0, 0%, 26%)'
+
 export const secondaryColor = 'hsl(34.3, 14%, 98%)'
+
 export const textColor = 'hsl(34, 78%, 91%)'
+
+export const whisper = 'hsl(30, 20%, 98%)'
