32307525faabd524263dc2c5128f611d5fda0a72
Titux Metal <github@lgdweb.fr>
2020-06-18 09:34:06 +0200

    Adding NavItems.js file in Main/styled; nav items styled component

---
+++ src/components/Main/styled/NavItems.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const NavItems = styled.ul`
+  align-items: center;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  margin: auto;
+  padding: 0;
+`
+
+export default NavItems
