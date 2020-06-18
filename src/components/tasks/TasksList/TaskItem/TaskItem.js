eba0db1b9feace7a0df2fa5defd8c2287e31cdbf
Titux Metal <github@lgdweb.fr>
2020-06-18 10:48:35 +0200

    Adding TaskItem.js file in tasks/TasksList/TaskItem; task item component

---
+++ src/components/tasks/TasksList/TaskItem/TaskItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { DeleteIcon, DoneIcon, EditIcon, UndoneIcon } from '~/components/tasks/shared'
+import { Box } from '~/components/tasks/styled'
+
+const Task = styled.p`
+  color: ${({ completed, theme }) => (completed ? theme.darkGoldenrod : theme.paleGoldenrod)};
+  cursor: pointer;
+  flex-grow: 1;
+  text-align: center;
+  text-decoration: ${({ completed }) => completed && 'line-through'};
+`
+
+const TaskItem = () => (
+  <Box>
+    <UndoneIcon />
+    <DoneIcon />
+    <Task>Task One</Task>
+    <EditIcon />
+    <DeleteIcon />
+  </Box>
+)
+
+export default TaskItem
