329717d5c0662830bc0e11fc8cfd43aeffcc2701
Titux Metal <github@lgdweb.fr>
2020-06-18 09:53:33 +0200

    Adding Title.js file in shared; title shared component

---
+++ src/components/shared/Title.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+  text-align: center;
+  margin: 1rem;
+  padding: 0;
+`
+
+export default Title
