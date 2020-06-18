259f431aa70f36ab30f1c6120d135988b140c001
Titux Metal <github@lgdweb.fr>
2020-06-18 09:16:37 +0200

    Updating GlobalStyle.js file in theme; removing duplicate body style and adding default style for ul

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -3,15 +3,6 @@ import { createGlobalStyle } from 'styled-components'
 import * as theme from './colors'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    padding: 0;
-    margin: 0;
-    font-size: 1rem;
-    line-height: 2;
-    background-color: ${theme.primaryColor};
-    color: ${theme.textColor};
-  }
-
   html { box-sizing: border-box; font-size: 20px; font-family: 'Roboto', sans-serif; }
   *, *:before, *:after { box-sizing: inherit; outline: none; }
   *::-moz-focus-inner { border: none; }
@@ -24,6 +15,8 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${theme.primaryColor};
     color: ${theme.textColor};
   }
+
+  ul { list-style-type: none; margin: 0; padding: 0; }
 `
 
 export default GlobalStyle
