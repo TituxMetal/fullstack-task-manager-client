532d25905d50b4e7f5bf00f26f4dd8541a98c09d
Titux Metal <github@lgdweb.fr>
2020-06-18 10:50:51 +0200

    Adding TasksList.js file in tasks/TasksList; tasks list component

---
+++ src/components/tasks/TasksList/TasksList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { TaskItem } from './TaskItem'
+
+const TasksList = () => (
+  <ul>
+    <li>
+      <TaskItem />
+    </li>
+    <li>
+      <TaskItem />
+    </li>
+    <li>
+      <TaskItem />
+    </li>
+  </ul>
+)
+
+export default TasksList
