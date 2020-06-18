7e32e467055621d3e0c6178e2ea57f1940fdc06a
Titux Metal <github@lgdweb.fr>
2020-06-18 10:44:36 +0200

    Adding EditIcon.js file in tasks/shared/Icons; edit icon component

---
+++ src/components/tasks/shared/Icons/EditIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Edit } from '@styled-icons/material'
+
+import { cornflowerBlue } from '~/theme/colors'
+import { Icon } from './Icon'
+
+export const EditIcon = () => (
+  <Icon color={cornflowerBlue}>
+    <Edit />
+  </Icon>
+)
+
+export default EditIcon
