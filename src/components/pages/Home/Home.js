238a203f0cdd9044802b50cbb677b05d13b362b9
Titux Metal <github@lgdweb.fr>
2021-05-29 16:02:10 +0200

    Reorganizing Home component in pages

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-import { Container, Title } from '#root/components/shared'
 import { Form, TasksList } from '#root/components/tasks'
 
+import { Title } from './styled'
+
 const Home = () => (
-  <Container>
+  <>
     <Title>Home Page</Title>
     <Form />
     <TasksList />
-  </Container>
+  </>
 )
 
 export default Home
---
+++ src/components/pages/Home/styled/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.textColor};
+  text-align: center;
+  margin: 1rem;
+  padding: 0;
+`
