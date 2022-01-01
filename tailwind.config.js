a34e601dd7a57076ce26c65b8cd6a4051356d317
Titux Metal (DEV) <github@lgdweb.fr>
2022-01-01 07:39:48 +0100

    Updating project setup files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  darkMode: 'class',
+  content: ['./src/**/*.{js, html}'],
   theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    extend: {
+      colors: {
+        neutral: colors.neutral[800],
+        primary: colors.yellow[600],
+        secondary: colors.amber[200],
+        amber: colors.amber,
+        yellow: colors.yellow,
+        gray: colors.neutral,
+        white: colors.neutral[50]
+      }
+    }
+  }
 }
