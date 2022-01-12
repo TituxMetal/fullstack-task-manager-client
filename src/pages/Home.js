8c4c58cdc72067b8e16e5547f471c6369de42a95
Titux Metal (DEV) <github@lgdweb.fr>
2022-01-12 10:07:04 +0100

    Updating Home.js file in pages; adds the query client provider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { TaskForm, TaskList } from '~/components'
 import { CurrentTaskProvider } from '~/hooks'
 
+const queryClient = new QueryClient()
+
 const HomePage = () => (
-  <CurrentTaskProvider>
-    <TaskForm />
-    <TaskList />
-  </CurrentTaskProvider>
+  <QueryClientProvider client={queryClient}>
+    <CurrentTaskProvider>
+      <TaskForm />
+      <TaskList />
+    </CurrentTaskProvider>
+  </QueryClientProvider>
 )
 
 export default HomePage
