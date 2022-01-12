f8601258eef1065dc8199de4b11230f4aa2608c2
Titux Metal (DEV) <github@lgdweb.fr>
2022-01-12 10:03:17 +0100

    Updating TaskItem.js file in components; adds functions for delete, edit and toggle completed operations

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
 import { Button, ItemContainer, ItemContent } from '~/components'
-import { useCurrentTask } from '~/hooks'
+import { useCurrentTask, useDeleteTask, useSaveTask, useTaskQuery } from '~/hooks'
 
-const TaskItem = ({ completed, description, id }) => {
+const TaskItem = ({ dataId }) => {
   const { setCurrent } = useCurrentTask()
+  const toggleCompleteMutation = useSaveTask()
+  const deleteTaskMutation = useDeleteTask()
+  const queryInfo = useTaskQuery(dataId)
+  const { completed, description, id } = queryInfo.data
 
-  const onDeleteHandler = () => console.log('deleting item with id', id)
+  const onEditHandler = () => setCurrent({ completed, description, id })
+  const onDeleteHandler = () => deleteTaskMutation.mutate(id)
+  const toggleCompletedHandler = () => toggleCompleteMutation.mutate({ completed: !completed, id })
 
   return (
     <ItemContainer>
-      {completed ? <Button.Done /> : <Button.Undone />}
-      <ItemContent $isDone={completed}>{description}</ItemContent>
-      <Button.Edit onClick={() => setCurrent({ completed, description, id })} />
+      <ItemContainer as='span' onClick={toggleCompletedHandler}>
+        {completed ? <Button.Done /> : <Button.Undone />}
+        <ItemContent $isDone={completed}>{description}</ItemContent>
+      </ItemContainer>
+      <Button.Edit onClick={onEditHandler} />
       <Button.Delete onClick={onDeleteHandler} />
     </ItemContainer>
   )
